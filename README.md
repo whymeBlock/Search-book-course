7d5526e1112e31221bac3147752e6305aa8c0f6a
whymeBlock <zhigailov42@gmail.com>
2023-12-23 19:50:43 +0700

    Edit READ.md

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Проект для практики
+Делал проект по [видео](https://www.youtube.com/watch?v=7xL9c39DhjI)
+Репозиторий автора - https://github.com/prabinmagar/booklib-app-using-react-js-and-openlib-api
+
+Изучил:
+- Архитектуру постраения данного проекта;
+- Принцип работы хука useContext;
+- Принцип работы хука useCallback
+- Принцип работы с переменными в **css**
--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -6,14 +6,14 @@ import "./SearchForm.css"
 
 const SearchForm = () => {
     const {setSearchTerm, setResultTitle} = useGlobalContext()
-    const searchText = useRef('')
+        const searchText = useRef('')
     const navigate = useNavigate()
 
     useEffect(() => {
         searchText.current.focus()
     }, []);
     const handleSubmit = (e) => {
-        e.preventDefault()
+        // e.preventDefault()
         let tempSearchTerm = searchText.current.value.trim()
         if ((tempSearchTerm.replace(/[^\w\s]/gi, "")).length === 0) {
             setSearchTerm("the lost world")
